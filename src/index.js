5f81537ed4d1c6207703b76e95425286898defb8
David Judge <dbiro97@gmail.com>
2025-04-05 18:28:37 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-// Adapted from https://github.com/babel/babel/tree/main/packages/babel-plugin-transform-react-display-name
 import { declare } from "@babel/helper-plugin-utils";
 import path from "path";
 import { types as t } from "@babel/core";
@@ -7,13 +6,19 @@ export default declare(api => {
   api.assertVersion(7);
 
   function addDisplayName(id, call) {
-    let objectAssignExpression = t.memberExpression(t.identifier("Object"), t.identifier("assign"));
-    let callExpression = t.callExpression(
-      objectAssignExpression, // We're calling 'Object.assign'.
-      [t.objectExpression([]), call, t.objectExpression([t.objectProperty(t.identifier("displayName"), t.stringLiteral(id))])] // Call 'Object.assign'.
+    // Extract the original arguments
+    const args = call.arguments;
+    
+    // Create the styled.withConfig call
+    const withConfigCall = t.callExpression(
+      t.memberExpression(call.callee, t.identifier("withConfig")),
+      [t.objectExpression([
+        t.objectProperty(t.identifier("displayName"), t.stringLiteral(id))
+      ])]
     );
-
-    return callExpression
+    
+    // Apply the original arguments to the result of withConfig
+    return t.callExpression(withConfigCall, args);
   }
 
   const isStyledFn = callee => callee.name === "styled";
@@ -22,16 +27,13 @@ export default declare(api => {
     if (!node || !t.isCallExpression(node)) return false;
     
     // not styled member object
-    if (
-      !isStyledFn(node.callee)
-      ) {
-        return false;
-      }
+    if (!isStyledFn(node.callee)) {
+      return false;
+    }
       
     // no call arguments
     const args = node.arguments;
     if (args.length < 2) return false;
-
 
     return true;
   }
@@ -41,7 +43,7 @@ export default declare(api => {
 
     visitor: {
       ExportDefaultDeclaration(nodePath, state) {
-        const { node } = nodePath
+        const { node } = nodePath;
         if (isStyled(node.declaration)) {
           const filename = state.filename || "unknown";
 
@@ -65,11 +67,13 @@ export default declare(api => {
         // crawl up the ancestry looking for possible candidates for displayName inference
         path.find(function (path) {
           if (path.isCallExpression()) {
-            if (path.node.callee.name === 'object', path.node.callee.property?.name === 'assign') {
-              if (path.node.arguments.some(node => node.type === 'ObjectExpression' && node.properties.some((prop) => prop.key.name === 'displayName'))) {
-                // displayName already assigned
-                return true
-              }
+            // Check if we already transformed this node
+            if (
+              path.node.callee.type === "MemberExpression" && 
+              path.node.callee.property?.name === "withConfig"
+            ) {
+              // displayName already assigned
+              return true;
             }
           } else if (path.isAssignmentExpression()) {
             id = path.node.left;
